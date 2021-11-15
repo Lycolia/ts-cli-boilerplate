8b6c409b8933057b2fae52fc4d673e423eb98cbd
Lycolia Rizzim <lycolia@lycolia.info>
2021-11-15 19:16:52 +0900

    replace ts-node to swc

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,15 @@ module.exports = {
   clearMocks: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  preset: 'ts-jest',
   clearMocks: true,
   testEnvironment: 'node',
   roots: ['<rootDir>/src/'],
   moduleFileExtensions: ['ts', 'js'],
   collectCoverageFrom: ['src/**/*.{ts,js}'],
   testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|dist)[/\\\\]'],
+  transform: {
+    '^.+\\.ts$': ['@swc/jest'],
+  },
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
